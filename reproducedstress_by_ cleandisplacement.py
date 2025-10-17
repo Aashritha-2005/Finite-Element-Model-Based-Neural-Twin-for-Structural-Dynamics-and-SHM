af55489c57ad8c1ac7c2598bd71c7c1ae9a3fa02
Aashritha-2005 <mallampatiaashritha29@gmail.com>
2025-10-17 07:31:17 +0530

    Add files via upload
    Removed unused imports and code comments.

--- reproducedstress_by_ cleandisplacement.py
+++ reproducedstress_by_ cleandisplacement.py
@@ -1,9 +1,3 @@
-# %%
-import marimo
-
-__generated_with = "0.1.0"
-app = marimo.App()
-
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
@@ -102,4 +96,4 @@ for i, case in enumerate([1, 2, 3, 4]):
             ax.legend(loc='lower left', frameon=True, borderpad=0.3)
 
 plt.show()
-# %%
+
